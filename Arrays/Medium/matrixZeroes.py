f2091616e07d3e02255fdd3bf0effacbd73fa8c9
Lokesh396 <lokeshkumarreddy.c@gmail.com>
2025-12-30 10:03:20 +0530

    matrixZeroes Matrix Zeroes problem was solved.

---
+++ Arrays/Medium/matrixZeroes.py
@@ -0,0 +1,54 @@
+import sys
+import os
+from pathlib import Path
+
+# Fast I/O and Recursion Setup
+sys.setrecursionlimit(2000)
+input = sys.stdin.readline
+
+USE_FILE = True
+
+if USE_FILE:
+    BASE_DIR = Path(__file__).resolve().parents[2]
+    sys.stdin = open(os.path.join(BASE_DIR, "input.txt"), "r")
+    sys.stdout = open(os.path.join(BASE_DIR, "output.txt"), "w")
+def setZeroes(matrix):
+       
+        m = len(matrix)
+        n = len(matrix[0])
+        col1 = 1
+
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    if j != 0:
+                        matrix[0][j] = 0
+                    else:
+                        col1 = 0
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        for i in range(1,n):
+            if matrix[0][0] == 0:
+                matrix[0][i] = 0
+        if col1 == 0:
+            for i in range(m):
+                matrix[i][0] = 0
+def main():
+    # -------------------------
+    # WRITE YOUR LOGIC BELOW
+    # -------------------------
+    m = int(input())
+    matrix = []
+    for i in range(m):
+        cols = list(map(int, input().split()))
+        matrix.append(cols)
+    setZeroes(matrix=matrix)
+    print('Matrix After Zeroes: ', matrix)
+    return 0
+
+if __name__ == "__main__":
+    # Note: These prints will go to output.txt if USE_FILE is True
+    main()
