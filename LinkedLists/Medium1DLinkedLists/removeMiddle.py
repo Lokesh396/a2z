46c079fc529c25ba1125d96f293a39dd2c38c21c
Lokesh396 <lokeshkumarreddy.c@gmail.com>
2026-01-17 20:14:52 +0530

    Day 25 LinkedLists Medium1DLinkedLists Two problems were solved 1 removeMiddle 2 removeNNode

---
+++ LinkedLists/Medium1DLinkedLists/removeMiddle.py
@@ -0,0 +1,49 @@
+import sys
+import os
+from pathlib import Path
+from typing import *
+
+# Fast I/O and Recursion Setup
+sys.setrecursionlimit(2000)
+input = sys.stdin.readline
+
+USE_FILE = True
+
+if USE_FILE:
+    BASE_DIR = Path(__file__).resolve().parents[2]
+    sys.stdin = open(os.path.join(BASE_DIR, "input.txt"), "r")
+    sys.stdout = open(os.path.join(BASE_DIR, "output.txt"), "w")
+
+# Definition for singly-linked list.
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        slow = fast = head
+        prev = None
+
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        if slow == fast:
+            return slow.next
+        prev.next = prev.next.next
+
+        return head
+
+def main():
+    # -------------------------
+    # WRITE YOUR LOGIC BELOW
+    # -------------------------
+    
+    return 0
+
+if __name__ == "__main__":
+    # Note: These prints will go to output.txt if USE_FILE is True
+    main()
