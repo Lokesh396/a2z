0618bbd8264901a481fb85fcb133fae779226590
Lokesh396 <lokeshkumarreddy.c@gmail.com>
2026-03-16 07:15:29 +0530

    Day 81 Binary Search Trees - Practice Problems Three problems were solved. 1 kth smallest element in a BST 2. Lowest Common Ancestor of a Binary Search Tree 3. Valid Binary Search Tree

---
+++ BinarySearchTrees/practiceProblems/kthSmallestBST.py
@@ -0,0 +1,50 @@
+import sys
+import os
+from pathlib import Path
+from typing import Optional
+
+# Fast I/O and Recursion Setup
+sys.setrecursionlimit(2000)
+input = sys.stdin.readline
+
+USE_FILE = True
+
+if USE_FILE:
+    BASE_DIR = Path(__file__).resolve().parents[2]
+    sys.stdin = open(os.path.join(BASE_DIR, "input.txt"), "r")
+    sys.stdout = open(os.path.join(BASE_DIR, "output.txt"), "w")
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def inorder(self,node):
+        if node:
+            self.inorder(node.left)
+            self.k -= 1
+
+            if self.k == 0:
+                self.ans = node.val
+                return
+            self.inorder(node.right)
+        
+
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        self.ans = None
+        self.k = k
+        self.inorder(root)
+        return self.ans
+
+def main():
+    # -------------------------
+    # WRITE YOUR LOGIC BELOW
+    # -------------------------
+    
+    return 0
+
+if __name__ == "__main__":
+    # Note: These prints will go to output.txt if USE_FILE is True
+    main()
---
+++ BinarySearchTrees/practiceProblems/validBST.py
@@ -0,0 +1,45 @@
+import sys
+import os
+from pathlib import Path
+from typing import Optional
+
+# Fast I/O and Recursion Setup
+sys.setrecursionlimit(2000)
+input = sys.stdin.readline
+
+USE_FILE = True
+
+if USE_FILE:
+    BASE_DIR = Path(__file__).resolve().parents[2]
+    sys.stdin = open(os.path.join(BASE_DIR, "input.txt"), "r")
+    sys.stdout = open(os.path.join(BASE_DIR, "output.txt"), "w")
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        
+        def validTree(root, minval, maxval):
+            if not root:
+                return True
+            
+            if not minval < root.val < maxval:
+                return False
+            
+            return validTree(root.left, minval, root.val) and validTree(root.right, root.val, maxval)
+        
+        return validTree(root, -float('inf'), float('inf'))
+
+def main():
+    # -------------------------
+    # WRITE YOUR LOGIC BELOW
+    # -------------------------
+    
+    return 0
+
+if __name__ == "__main__":
+    # Note: These prints will go to output.txt if USE_FILE is True
+    main()
