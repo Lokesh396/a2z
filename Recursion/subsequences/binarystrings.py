811aeb97059de11dd66ec54c6468165b3ac95bdf
Lokesh396 <lokeshkumarreddy.c@gmail.com>
2026-03-20 06:28:30 +0530

    Two problems were revised.

--- Recursion/subsequences/binarystrings.py
+++ Recursion/subsequences/binarystrings.py
@@ -15,6 +15,28 @@ if USE_FILE:
     sys.stdout = open(os.path.join(BASE_DIR, "output.txt"), "w")
 
 def allstrings(prev, ans, n):
+    """
+    we need to generate all the substring of size n, where there will
+    be no consective 0 in the string.
+
+    Algorithm:
+     - the base cases are if we the current generated string length is 2, we need to verify
+     whether the last two characthers contains only zeroes, if they are we need to reutrn early
+     - if we reach n so that means we crated one valid substring of size we append that to our ans
+     and return.
+     - we have two choices either we can add 0 or 1 to our already previous string.
+
+     Args:
+        prev : string that constructed so far
+        ans: lst to store the answer
+        n: legnth of the substring.
+
+    Returns: returns the vaid substrings.
+
+    Time Complexity: O(2^n)
+
+    Space Complexity :O(n)
+    """
     if len(prev) >= 2:
         if prev[-1] == '0' and prev[-2] == '0':
             return
--- Recursion/subsequences/powerSet.py
+++ Recursion/subsequences/powerSet.py
@@ -15,14 +15,37 @@ if USE_FILE:
     sys.stdout = open(os.path.join(BASE_DIR, "output.txt"), "w")
 
 def generate( nums, idx, curr, ans):
+    """
+
+    We are given an list of size n, we generate all the possible subsequences.
+
+    Algorithm:
+        - at every step we have two choices take the current element or skip it
+        - we backtrack everytime after take the element we remove it from our solution.
+        - if the current index equal len(nums), a solution has been constructed, we
+        append that to our solution and returns.
+    
+    Args:
+        nums: input array
+        idx : curr index
+        curr: current formed solution
+        ans: list storing subsequences
+
+    Returns: return the array of subsequences
+
+    Time Complexity: O(2^n)
+
+    Space Complexity: O(n)
+
+    """
     if idx >= len(nums):
         ans.append(curr[::])
         return
     
-    generate(nums, idx+1, curr, ans)
     curr.append(nums[idx])
     generate(nums, idx+1, curr, ans)
     curr.pop()
+    generate(nums, idx+1, curr, ans)
 def subsets( nums: List[int]) -> List[List[int]]:
     ans = []
     generate(nums, 0, [], ans)
@@ -33,7 +56,9 @@ def main():
     # WRITE YOUR LOGIC BELOW
     # -------------------------
     nums = list(map(int, input().split()))
-    print(f'All subsets for {nums}:',subsets(nums))
+    subset = subsets(nums)
+    print(len(subset))
+    print(f'All subsets for {nums}:',subset)
     return 0
 
 if __name__ == "__main__":
