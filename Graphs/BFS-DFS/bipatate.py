1bd8c6cb1370ed5999cb9f8f00ace099ef486d8e
Lokesh396 <lokeshkumarreddy.c@gmail.com>
2026-03-24 07:24:34 +0530

    Day 89, Graph Traversals. two problems were solved. 1 islands 2 bipatate

---
+++ Graphs/BFS-DFS/bipatate.py
@@ -0,0 +1,62 @@
+import sys
+import os
+from pathlib import Path
+from typing import List
+from collections import defaultdict
+
+# Fast I/O and Recursion Setup
+sys.setrecursionlimit(2000)
+input = sys.stdin.readline
+
+USE_FILE = True
+
+if USE_FILE:
+    BASE_DIR = Path(__file__).resolve().parents[2]
+    sys.stdin = open(os.path.join(BASE_DIR, "input.txt"), "r")
+    sys.stdout = open(os.path.join(BASE_DIR, "output.txt"), "w")
+
+class Solution:
+    '''
+    missed to check not ocnnected components
+    we need to wait for all children before returning
+    '''
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        adjList = defaultdict(list)
+        for idx, val in enumerate(graph):
+            adjList[idx] = val
+        
+
+        visited = [-1 for i in range(len(graph))]
+
+        def dfs(i, c):
+            visited[i] = c
+
+            for child in adjList[i]:
+                nc = 1 if c == 0 else 0
+                if visited[child] != -1:
+                    if visited[child] == c:
+                        return False
+                else:
+                    if not dfs(child, nc):
+                        return False
+            
+            return True
+
+        for i in range(len(graph)):
+            if visited[i] == -1:
+                if not dfs(i, 0):
+                    return False
+    
+        return True
+            
+
+def main():
+    # -------------------------
+    # WRITE YOUR LOGIC BELOW
+    # -------------------------
+    
+    return 0
+
+if __name__ == "__main__":
+    # Note: These prints will go to output.txt if USE_FILE is True
+    main()
---
+++ Graphs/BFS-DFS/islands.py
@@ -0,0 +1,49 @@
+import sys
+import os
+from pathlib import Path
+from typing import List
+
+# Fast I/O and Recursion Setup
+sys.setrecursionlimit(2000)
+input = sys.stdin.readline
+
+USE_FILE = True
+
+if USE_FILE:
+    BASE_DIR = Path(__file__).resolve().parents[2]
+    sys.stdin = open(os.path.join(BASE_DIR, "input.txt"), "r")
+    sys.stdout = open(os.path.join(BASE_DIR, "output.txt"), "w")
+
+def numIslands(self, grid: List[List[str]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        def traverse(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == '0':
+                return
+            
+            grid[i][j] = '0'
+
+            for dx, dy in [[-1, 0], [0, -1], [0, 1], [1, 0]]:
+                x = i + dx
+                y = j + dy
+
+                traverse(x, y)
+        
+        cnt = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    traverse(i, j)
+                    cnt += 1
+        
+        return cnt
+def main():
+    # -------------------------
+    # WRITE YOUR LOGIC BELOW
+    # -------------------------
+    
+    return 0
+
+if __name__ == "__main__":
+    # Note: These prints will go to output.txt if USE_FILE is True
+    main()
